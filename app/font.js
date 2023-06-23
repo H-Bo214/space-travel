e6d6a172a31874977cac30695a58fa6bb6d55d26
Horacio Borrego <mac-m1@H-M1.local>
2023-06-23 10:36:12 -0700

    Add project fonts to font.js

---
+++ app/font.js
@@ -0,0 +1,11 @@
+import { Bellefair, Barlow_Condensed } from 'next/font/google'
+
+export const bellefair = Bellefair({ 
+  weight: ['400'],
+  subsets: ['latin'] 
+})
+
+export const barlowCondensed = Barlow_Condensed({ 
+  weight: ['400', '700'],
+  subsets: ['latin'] 
+})
