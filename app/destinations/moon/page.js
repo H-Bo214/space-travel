bd11eccdfda7d9cbd489ab897000264d0501f4da
Horacio Borrego <mac-m1@H-M1.local>
2023-08-07 10:11:46 -0700

    Called getDestinations to render Moon destination data

--- app/destinations/moon/page.js
+++ app/destinations/moon/page.js
@@ -1,16 +1,21 @@
-import destinations from "../../../data.json";
-import Planet from "../../Planet/page.js";
+import Destination from "../../Destination/page.js";
+import getDestinations from "../../apiCalls.js";
 
-export default function Moon() {
-  console.log("destinations", destinations);
+export default async function Moon() {
+  const destinations = await getDestinations();
+  const { name, description, distance, travel, images } = destinations.find(
+    (destination) => destination.name === "moon"
+  );
+  const { png } = images;
   return (
     <>
-      <Planet
-        name={destinations.destinations[0].name}
-        image={destinations.destinations[0].images.png}
-        description={destinations.destinations[0].description}
-        distance={destinations.destinations[0].distance}
-        travel={destinations.destinations[0].travel}
+      <h1>Moon route</h1>
+      <Destination
+        name={name}
+        image={png}
+        description={description}
+        distance={distance}
+        travel={travel}
       />
     </>
   );
