9d49d0a43d845556cb920a1627863ba5dd9195b9
Horacio Borrego <mac-m1@H-M1.local>
2023-08-11 10:49:06 -0700

    Add Europa file route, loading, error pages

---
+++ app/destinations/europa/page.js
@@ -0,0 +1,20 @@
+import getDestinations from '@/app/apiCalls';
+import Destination from '@/app/Destination/page';
+
+export default async function Europa() {
+  const destinations = await getDestinations();
+  const { name, description, distance, travel, images } = destinations.find(
+    (destination) => destination.name === 'europa'
+  );
+  return (
+    <>
+      <Destination
+        name={name}
+        image={images.png}
+        description={description}
+        distance={distance}
+        travel={travel}
+      />
+    </>
+  );
+}
