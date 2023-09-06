5ce36f1ac8bf78c158ae023eacea07cb796ef49e
Horacio Borrego <mac-m1@H-M1.local>
2023-09-06 16:03:07 -0700

    Update commander file route path to display commander details rendering a CrewMember component

--- app/crew/commander/page.js
+++ app/crew/commander/page.js
@@ -1,15 +1,15 @@
-import { getDestinations } from '../../apiCalls.js';
+import { getDestinations, getCrew } from '../../apiCalls.js';
 import CrewMember from '../../CrewMember/page.js';
 
-export default function Commander() {
-  // const destinations = await getDestinations();
-  // const { name, description, distance, travel, images } = destinations.find(
-  //   (destination) => destination.name === 'europa'
-  // );
+export default async function Commander() {
+  const crew = await getCrew();
+  console.log('crew', crew);
+  const { name, images, role, bio } = crew.find(
+    (member) => member.role === 'Commander'
+  );
   return (
     <>
-      <h1>Commander page</h1>
-      <CrewMember />
+      <CrewMember name={name} role={role} bio={bio} images={images} />
     </>
   );
 }
