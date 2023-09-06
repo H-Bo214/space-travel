571aaa98b49a783de6e1d45f7fd9c85da2dc5504
Horacio Borrego <mac-m1@H-M1.local>
2023-09-06 16:05:41 -0700

    Create file route path for mission-specialist and render details when visiting this route

---
+++ app/crew/mission-specialist/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  // Create a loading.js file in each directory to handle content loading delays and display information to the user.
+  return (
+    <div>
+      <h1>Loading.....</h1>
+    </div>
+  )
+}
