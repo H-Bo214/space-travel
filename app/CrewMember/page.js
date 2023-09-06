2e58507cce4191eba713d5626bc318f1d6957569
Horacio Borrego <mac-m1@H-M1.local>
2023-09-06 16:04:39 -0700

    Update CrewMember component to accept entire images object to utilize height and width of images in Next.js Image component

--- app/CrewMember/page.js
+++ app/CrewMember/page.js
@@ -2,10 +2,27 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import { bellefair, barlowCondensed, barlow } from '../font';
 
-export default function CrewMember() {
+export default function CrewMember({ name, images, role, bio }) {
+  console.log('images', images)
   return (
     <section className={styles.container}>
-      <h1>CrewMember Component</h1>
+      <article className={styles.info}>
+        <h2 className={`${bellefair.className} ${styles.role}`}>{role}</h2>
+        <h3 className={`${bellefair.className} ${styles.name}`}>{name}</h3>
+        <p className={`${barlow.className} ${styles.bio}`}>{bio}</p>
+      </article>
+      <section className={styles.imageContainer}>
+        <Image
+          className={styles.image}
+          src={images.png}
+          width={images.width}
+          height={images.height}
+          // fill={true}
+          alt={name}
+          placeholder='blur'
+          blurDataURL={images.png}
+        />
+      </section>
     </section>
   );
 }
